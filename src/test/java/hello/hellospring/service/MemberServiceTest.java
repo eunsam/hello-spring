91b1301474a2dd4f82de3d3d95b7be8fc3d8512e
eunsam <caxeo12@naver.com>
2020-12-29 21:43:15 +0900

    Jdbc+jpa

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -32,15 +32,16 @@ class MemberServiceTest {
     }
 
     @Test
-    public void 중복_회원_예외() {
-
+    public void 중복_회원_예외() throws Exception {
+        Member member1 = new Member();
+        member1.setName("spring");
+        Member member2 = new Member();
+        member2.setName("spring");
+        //When
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다.
+        Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
     }
 
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
 }
